fa31e3202c3830b32efbcffb4381a075eeca1064
Edwin Marrima <edwin.marrima@vm.co.mz>
2023-08-17 08:48:26 +0200

    wip: add comments

--- pkg/lang/types/function.go
+++ pkg/lang/types/function.go
@@ -20,12 +20,13 @@ func New(spec FuncSpecification) Function {
 // conform to the function's parameter specification or an error will be returned.
 func (f Function) Call(args ...any) (FuncReturn, error) {
 	if len(args) != len(f.spec.Params) {
-		return FuncReturn{}, NewWrongFunctionParametersError(f.spec.Name, f.expectedParametersToString(), f.sentParametersToString(args))
+		return FuncReturn{}, NewWrongFunctionParametersError(f.spec.Name, f.convertExpectedParametersToString(), f.convertSentParametersToString(args))
 	}
 	return f.spec.Implementation(args)
 }
 
-func (f Function) sentParametersToString(parameters []any) string {
+// convertSentParametersToString transforms a list of sent parameters into a formatted string for use in error messages.
+func (f Function) convertSentParametersToString(parameters []any) string {
 	argsString := fmt.Sprintf("%s(", f.spec.Name)
 	for i, arg := range parameters {
 		if i > 0 {
@@ -36,7 +37,9 @@ func (f Function) sentParametersToString(parameters []any) string {
 	}
 	return argsString + ")"
 }
-func (f Function) expectedParametersToString() string {
+
+// convertExpectedParametersToString transforms a list of expected parameters into a formatted string for use in error messages.
+func (f Function) convertExpectedParametersToString() string {
 	paramsString := fmt.Sprintf("%s(", f.spec.Name)
 	for i, p := range f.spec.Params {
 		if i > 0 {
