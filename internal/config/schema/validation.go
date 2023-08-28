9dd8eb78ba1c7d903c84c8434311c99ffd400cc8
Edwin Marrima <edwin.marrima@vm.co.mz>
2023-08-28 20:56:02 +0200

    feat: structure configuration file validation package

--- internal/config/schema/validation.go
+++ internal/config/schema/validation.go
@@ -1,1 +1,33 @@
 package schema
+
+import (
+	"github.com/xeipuuv/gojsonschema"
+)
+
+const documentLoaderPath = "file://json_schema/main.json"
+
+func Validate(schemaPath string) (bool, error) {
+	sl := gojsonschema.NewSchemaLoader()
+
+	documentLoader := gojsonschema.NewReferenceLoader(schemaPath)
+
+	schemaLoader := gojsonschema.NewReferenceLoader(documentLoaderPath)
+	schemaLoader2 := gojsonschema.NewReferenceLoader("file://json_schema/provider.json")
+	err := sl.AddSchemas(schemaLoader, schemaLoader2)
+	if err != nil {
+		return false, err
+	}
+	schema, err := sl.Compile(schemaLoader)
+	if err != nil {
+		return false, err
+	}
+	result, err := schema.Validate(documentLoader)
+	if err != nil {
+		return false, err
+	}
+	if result.Valid() {
+		return true, err
+	}
+
+	return false, nil
+}
--- internal/config/schema/validation_test.go
+++ internal/config/schema/validation_test.go
@@ -1,1 +1,28 @@
 package schema
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestValidate(t *testing.T) {
+	tests := []struct {
+		description     string
+		document        string
+		expectedOutcome interface{}
+	}{
+		{
+			description:     "Must return true when provided configuration File is valid",
+			document:        "file://test_artifacts/provider_validation_001.json",
+			expectedOutcome: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.description, func(t *testing.T) {
+			b, _ := Validate(tt.document)
+			if !reflect.DeepEqual(b, tt.expectedOutcome) {
+				t.Errorf("Result was incorrect.\ngot : %t\nwant: %t", b, tt.expectedOutcome)
+			}
+		})
+	}
+}
