45c38aa84e729718812dcde90a5c6c8fd7af66d2
Edwin Marrima <edwin.marrima@vm.co.mz>
2023-09-23 16:07:47 +0200

    feat: provider test

--- internal/tf-plan/tfplan.go
+++ internal/tf-plan/tfplan.go
@@ -6,15 +6,14 @@ import (
 )
 
 type PlanFile struct {
-	FormatVersion string        `json:"format_version"`
-	PlannedValues PlannedValues `json:"planned_values"`
+	FormatVersion string              `json:"format_version"`
+	Resources     map[string]Resource `json:"resources"`
+	// ModuleRemovedResources contains resources whose definition was deleted from the mods
+	ModuleRemovedResources map[string]Resource
+	// ProviderConfiguration holds provider configuration
+	ProviderConfiguration map[string]ProviderConfiguration
 }
-type PlannedValues struct {
-	RootModule `json:"root_module"`
-}
-type RootModule struct {
-	Resources []Resource `json:"resources"`
-}
+
 type Resource struct {
 	Address         string                 `json:"address"`
 	Type            string                 `json:"type"`
@@ -30,13 +29,6 @@ type ProviderConfiguration struct {
 	Name              string
 	FullName          string
 	VersionConstraint string
-	Expressions       map[string][]ExpressionConfig
-}
-
-type ExpressionConfig map[string]References
-
-type References struct {
-	References []interface{}
 }
 
 func ParsePlanFile(planFilepath string) (*PlanFile, error) {
@@ -45,9 +37,88 @@ func ParsePlanFile(planFilepath string) (*PlanFile, error) {
 		return nil, err
 	}
 	var tfplan PlanFile
-	if err = json.Unmarshal(data, &tfplan); err != nil {
+
+	var planFile map[string]interface{}
+	if err = json.Unmarshal(data, &planFile); err != nil {
 		return nil, err
 	}
-
+	tfplan.FormatVersion = planFile["format_version"].(string)
+	tfplan.Resources, tfplan.ModuleRemovedResources = extractModuleResources(planFile)
+	tfplan.ProviderConfiguration = extractProvider(planFile)
 	return &tfplan, nil
+}
+func extractProvider(planFile map[string]interface{}) map[string]ProviderConfiguration {
+	provider := make(map[string]ProviderConfiguration)
+
+	return provider
+}
+func extractModuleResources(planFile map[string]interface{}) (map[string]Resource, map[string]Resource) {
+	plannedResources := make(map[string]Resource)
+	deletedResources := make(map[string]Resource)
+	plannedValues := planFile["planned_values"].(map[string]interface{})
+	rootModule := plannedValues["root_module"].(map[string]interface{})
+
+	//read root module resources
+	for _, resource := range rootModule["resources"].([]interface{}) {
+		r := resource.(map[string]interface{})
+		plannedResources[r["address"].(string)] = Resource{
+			Address:         r["address"].(string),
+			Type:            r["type"].(string),
+			Name:            r["name"].(string),
+			ProviderName:    r["provider_name"].(string),
+			Values:          r["values"].(map[string]interface{}),
+			SensitiveValues: r["sensitive_values"].(map[string]interface{}),
+		}
+	}
+
+	//read sub_modules resources
+	childModules := rootModule["child_modules"]
+	for _, childModule := range childModules.([]interface{}) {
+		ch := childModule.(map[string]interface{})["resources"].([]interface{})
+		for _, resource := range ch {
+			r := resource.(map[string]interface{})
+			plannedResources[r["address"].(string)] = Resource{
+				Address:         r["address"].(string),
+				Type:            r["type"].(string),
+				Name:            r["name"].(string),
+				ProviderName:    r["provider_name"].(string),
+				Values:          r["values"].(map[string]interface{}),
+				SensitiveValues: r["sensitive_values"].(map[string]interface{}),
+			}
+		}
+	}
+
+	//read actions
+	for _, resourceChanges := range planFile["resource_changes"].([]interface{}) {
+		rc := resourceChanges.(map[string]interface{})
+		change := rc["change"].(map[string]interface{})
+		actions := change["actions"].([]interface{})
+
+		address := rc["address"].(string)
+		if v, found := plannedResources[address]; found {
+			for _, action := range actions {
+				v.Actions = append(v.Actions, action.(string))
+			}
+			plannedResources[address] = v
+		}
+	}
+	// retrieve module erased resources
+	priorState := planFile["prior_state"].(map[string]interface{})
+	values := priorState["values"].(map[string]interface{})
+	rootModule = values["root_module"].(map[string]interface{})
+	for _, resource := range rootModule["resources"].([]interface{}) {
+		rc := resource.(map[string]interface{})
+		if _, found := plannedResources[rc["address"].(string)]; !found {
+			deletedResources[rc["address"].(string)] = Resource{
+				Address:         rc["address"].(string),
+				Type:            rc["type"].(string),
+				Name:            rc["name"].(string),
+				Actions:         []string{"delete"},
+				ProviderName:    rc["provider_name"].(string),
+				Values:          rc["values"].(map[string]interface{}),
+				SensitiveValues: rc["sensitive_values"].(map[string]interface{}),
+			}
+		}
+	}
+	return plannedResources, deletedResources
 }
--- internal/tf-plan/tfplan_test.go
+++ internal/tf-plan/tfplan_test.go
@@ -16,66 +16,122 @@ func TestParsePlanFile(t *testing.T) {
 			description:   "-",
 			inputFilePath: "testFile/1.json",
 			expectedOutcome: PlanFile{
+				ProviderConfiguration: map[string]ProviderConfiguration{
+					"aws": {
+						Name:              "aws",
+						FullName:          "registry.terraform.io/hashicorp/aws",
+						VersionConstraint: "~> 4.0",
+					},
+				},
 				FormatVersion: "1.2",
-				PlannedValues: PlannedValues{
-					RootModule: RootModule{
-						Resources: []Resource{
-							{
-								Address:      "aws_cloudwatch_log_group.yada",
-								Type:         "aws_cloudwatch_log_group",
-								Name:         "yada",
-								ProviderName: "registry.terraform.io/hashicorp/aws",
-								Values: map[string]interface{}{
-									"kms_key_id":        "arn:sadsdsdsdsdkl",
-									"name":              "Yada",
-									"retention_in_days": float64(5),
-									"skip_destroy":      false,
-									"tags": map[string]interface{}{
-										"Application": "serviceA",
-										"Environment": "production",
-									},
-									"tags_all": map[string]interface{}{
-										"Application": "serviceA",
-										"Environment": "production",
-									},
-								},
-								SensitiveValues: map[string]interface{}{
-									"tags":     map[string]interface{}{},
-									"tags_all": map[string]interface{}{},
-								},
+				ModuleRemovedResources: map[string]Resource{
+					"null_resource.cluster_dummy": {
+						Address:      "null_resource.cluster_dummy",
+						Type:         "null_resource",
+						Name:         "cluster_dummy",
+						ProviderName: "registry.terraform.io/hashicorp/null",
+						Actions:      []string{"delete"},
+						Values: map[string]interface{}{
+							"id": "1580278013551376455",
+							"triggers": map[string]interface{}{
+								"cluster_instance_ids": "aaaaa",
 							},
-							{
-								Address:      "aws_s3_bucket.example",
-								Type:         "aws_s3_bucket",
-								Name:         "example",
-								ProviderName: "registry.terraform.io/hashicorp/aws",
-								Values: map[string]interface{}{
-									"bucket":        "my-tf-test-bucket",
-									"force_destroy": false,
-									"tags": map[string]interface{}{
-										"Environment": "Dev",
-										"Name":        "My bucket",
-									},
-									"tags_all": map[string]interface{}{
-										"Environment": "Dev",
-										"Name":        "My bucket",
-									},
-									"timeouts": nil,
-								},
-								SensitiveValues: map[string]interface{}{
-									"cors_rule":                            []any{},
-									"grant":                                []any{},
-									"lifecycle_rule":                       []any{},
-									"logging":                              []any{},
-									"object_lock_configuration":            []any{},
-									"replication_configuration":            []any{},
-									"server_side_encryption_configuration": []any{},
-									"tags":                                 map[string]interface{}{},
-									"tags_all":                             map[string]interface{}{},
-									"versioning":                           []any{},
-									"website":                              []any{},
-								},
+						},
+						SensitiveValues: map[string]interface{}{
+							"triggers": map[string]interface{}{},
+						},
+					},
+				},
+				Resources: map[string]Resource{
+					"aws_cloudwatch_log_group.yada": {
+						Address:      "aws_cloudwatch_log_group.yada",
+						Type:         "aws_cloudwatch_log_group",
+						Name:         "yada",
+						ProviderName: "registry.terraform.io/hashicorp/aws",
+						Actions:      []string{"create"},
+						Values: map[string]interface{}{
+							"kms_key_id":        "arn:sadsdsdsdsdkl",
+							"name":              "Yada",
+							"retention_in_days": float64(5),
+							"skip_destroy":      false,
+							"tags": map[string]interface{}{
+								"Application": "serviceA",
+								"Environment": "production",
 							},
+							"tags_all": map[string]interface{}{
+								"Application": "serviceA",
+								"Environment": "production",
+							},
+						},
+						SensitiveValues: map[string]interface{}{
+							"tags":     map[string]interface{}{},
+							"tags_all": map[string]interface{}{},
+						},
+					},
+					"aws_s3_bucket.example": {
+						Address:      "aws_s3_bucket.example",
+						Type:         "aws_s3_bucket",
+						Name:         "example",
+						ProviderName: "registry.terraform.io/hashicorp/aws",
+						Actions:      []string{"create"},
+						Values: map[string]interface{}{
+							"bucket":        "aaaaa-dev",
+							"force_destroy": false,
+							"tags": map[string]interface{}{
+								"Environment": "Dev",
+								"Name":        "My bucket",
+							},
+							"tags_all": map[string]interface{}{
+								"Environment": "Dev",
+								"Name":        "My bucket",
+							},
+							"timeouts": nil,
+						},
+						SensitiveValues: map[string]interface{}{
+							"cors_rule":                            []any{},
+							"grant":                                []any{},
+							"lifecycle_rule":                       []any{},
+							"logging":                              []any{},
+							"object_lock_configuration":            []any{},
+							"replication_configuration":            []any{},
+							"server_side_encryption_configuration": []any{},
+							"tags":                                 map[string]interface{}{},
+							"tags_all":                             map[string]interface{}{},
+							"versioning":                           []any{},
+							"website":                              []any{},
+						},
+					},
+					"module.bucket_provisioner.aws_s3_bucket.example": {
+						Address:      "module.bucket_provisioner.aws_s3_bucket.example",
+						Type:         "aws_s3_bucket",
+						Name:         "example",
+						Actions:      []string{"create"},
+						ProviderName: "registry.terraform.io/hashicorp/aws",
+						Values: map[string]interface{}{
+							"bucket":        "bucket-test",
+							"force_destroy": false,
+							"tags": map[string]interface{}{
+								"Environment": "Dev",
+								"Name":        "My bucket",
+							},
+							"tags_all": map[string]interface{}{
+								"Environment": "Dev",
+								"Name":        "My bucket",
+							},
+							"timeouts": nil,
+						},
+						SensitiveValues: map[string]interface{}{
+							"cors_rule":                            []any{},
+							"grant":                                []any{},
+							"lifecycle_rule":                       []any{},
+							"logging":                              []any{},
+							"object_lock_configuration":            []any{},
+							"replication_configuration":            []any{},
+							"server_side_encryption_configuration": []any{},
+							"tags":                                 map[string]interface{}{},
+							"tags_all":                             map[string]interface{}{},
+							"versioning":                           []any{},
+							"website":                              []any{},
 						},
 					},
 				},
